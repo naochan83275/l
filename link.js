1c4c8e752571741f23f747d895cad05d6cc30409
なおちゃん <166379976+naochan83275@users.noreply.github.com>
2024-06-17 18:01:38 +0900

    Update link.js

--- link.js
+++ link.js
@@ -2,4 +2,4 @@
 // var [id] = '[リンク先url(https:// から)]'; の形で記述
 // 例: var hoge = 'https://example.com';
 
-var W2W0D2C4 = 
+var W2W0D2C4A = 'https://forms.office.com/Pages/AnalysisPage.aspx?AnalyzerToken=Y0khmarG2JqQwpU5IvUHlmpH2kdsKVVP&id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAANAATrCom5UM1RRSDFXMjlQVE1OS1U0VUxISk5JUldYTi4u'
