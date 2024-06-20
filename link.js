73b7750d986a53916b29678962976f6a4e5cbae0
なおちゃん <166379976+naochan83275@users.noreply.github.com>
2024-06-20 18:09:47 +0900

    Update link.js

--- link.js
+++ link.js
@@ -3,3 +3,4 @@
 // 例: var hoge = 'https://example.com';
 
 var W2W0D2C4A = 'https://forms.office.com/Pages/AnalysisPage.aspx?AnalyzerToken=Y0khmarG2JqQwpU5IvUHlmpH2kdsKVVP&id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAANAATrCom5UM1RRSDFXMjlQVE1OS1U0VUxISk5JUldYTi4u'
+var test = "javascript:alert('リンクサービスは正常に作動しています。')";
