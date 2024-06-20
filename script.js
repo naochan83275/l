d2231296238633d5ec3d584bfbf1e9c123057647
なおちゃん <166379976+naochan83275@users.noreply.github.com>
2024-06-20 17:17:55 +0900

    わざわざ外部に書かなくていいので削除
    スクリプトはindex.htmlファイル内に移動しました。

--- script.js
+++
@@ -1,9 +0,0 @@
-var url = window.location.href;
-if ( ~url.indexOf('https://naochan83275.github.io/l/#')){
-  var linkid = url.replace('https://naochan83275.github.io/l/#', '');
-  var link = linkid;
-  console.log = link;
-  window.location.href = link;
-}else{
-  window.location.href = 'https://naochan83275.github.io';
-}
